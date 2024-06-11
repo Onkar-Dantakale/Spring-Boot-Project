10494fe5f37317800b61c5a469149d0c9b4c9ee1
Onkar-Dantakale <onkardantkale1007@gmail.com>
2024-06-12 03:18:42 +0530

    modification in customer_service api

--- Customer_Service/src/main/java/com/example/app/model/Customer.java
+++ Customer_Service/src/main/java/com/example/app/model/Customer.java
@@ -5,7 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "Customer_Details")
 public class Customer {
@@ -15,29 +21,15 @@ public class Customer {
 	private long id;
 	private String name;
 	private String email;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", name=" + name + ", email=" + email + "]";
-	}
-	
+	/*
+	 * public long getId() { return id; } public void setId(long id) { this.id = id;
+	 * } public String getName() { return name; } public void setName(String name) {
+	 * this.name = name; } public String getEmail() { return email; } public void
+	 * setEmail(String email) { this.email = email; }
+	 * 
+	 * @Override public String toString() { return "Customer [id=" + id + ", name="
+	 * + name + ", email=" + email + "]"; }
+	 */
 	
 	
 	
